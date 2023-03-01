35292079092e8b9121ea8f54309aed4ef2cf2b36
Adel-Al-Qosi <Adel.Al.Qosi@gmail.com>
2023-03-01 15:17:45 +0300

    finish practice

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
 // Your code here
+const Employee = require('./employee')
+
+const employee = new Employee('Jhon Wick', 'Dog Lover')
+
+employee.sayName()
+
+const sayEmployee = employee.sayName.bind(employee)
+setTimeout(sayEmployee, 2000)
+
+const sayEmployeeOccupation = employee.sayOccupation.bind(employee)
+setTimeout(sayEmployeeOccupation, 3000)
